ef9f0837eff6576024ba5e5cc3a911004ad10fce
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-08-20 19:57:23 +0900

    [add/#1] MainBottomBar - key 임포트 추가

--- app/src/main/java/com/moive/app/presentation/main/component/MainBottomBar.kt
+++ app/src/main/java/com/moive/app/presentation/main/component/MainBottomBar.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
