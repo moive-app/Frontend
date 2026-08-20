4ab631881c00fe4472d93ed7efc7288ebe30d47f
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-08-20 19:51:08 +0900

    [add/#1] homeGraph, myPageGraph 임포트 추가

--- app/src/main/java/com/moive/app/presentation/main/MainNavHost.kt
+++ app/src/main/java/com/moive/app/presentation/main/MainNavHost.kt
@@ -3,6 +3,8 @@ package com.moive.app.presentation.main
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
+import com.moive.app.presentation.home.navigation.homeGraph
+import com.moive.app.presentation.mypage.navigation.myPageGraph
 
 @Composable
 fun MainNavHost(
