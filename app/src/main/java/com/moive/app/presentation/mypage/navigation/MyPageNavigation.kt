a8a281c1ceaaae98574782bfd8663915cde09125
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-08-20 19:52:05 +0900

    [add/#1] MyPageRoute 임포트 추가

--- app/src/main/java/com/moive/app/presentation/mypage/navigation/MyPageNavigation.kt
+++ app/src/main/java/com/moive/app/presentation/mypage/navigation/MyPageNavigation.kt
@@ -8,6 +8,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.moive.app.core.navigation.MainTabRoute
+import com.moive.app.presentation.mypage.MyPageRoute
 import kotlinx.serialization.Serializable
 
 fun NavController.navigateToMyPage(
